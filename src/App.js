45dcd3e17624d42dd90f0f0921f43074fa474174
fadilrm <mubarok881@gmail.com>
2022-10-28 12:20:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header/> 
+        <h1 className=''>HEllo</h1>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div>
+            <h1>Selamat datang </h1>
+        </div>
+    );
+}
+
+export default Header;

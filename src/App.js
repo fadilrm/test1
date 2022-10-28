91df77ccef750af7c7b87a989f73d0372ffae327
fadilrm <mubarok881@gmail.com>
2022-10-29 00:27:19 +0700

    membuat halaman dan Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './Page/Home';
+import Profile from './Page/Profile';
+import SignIn from './Page/SignIn';
+import SignUp from './Page/SignUp';
+import ForgotPassword from './Page/ForgotPassword';
+import Offers from './Page/Offers';
 
 function App() {
   return (
-    <div>
-      <Header/> 
-        <h1 className=''>HEllo</h1>
-  
-    </div>
+    <>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/Sign-In" element={<SignIn/>} />
+        <Route path="/Sign-Up" element={<SignUp/>} />
+        <Route path="/Forgot-Password" element={<ForgotPassword/>} />
+        <Route path="/Offers" element={<Offers/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
